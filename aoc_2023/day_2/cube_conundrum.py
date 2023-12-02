592bff2655e62d841259d7a6cb35088fe4563c80
Ben Lu <ayroblu@gmail.com>
2023-12-02 10:00:08 +0000

    day 2

---
+++ aoc_2023/day_2/cube_conundrum.py
@@ -0,0 +1,36 @@
+from utils.file import read_file
+import re
+
+from utils.types import not_none
+
+contents = read_file(__file__, "input")
+lines = contents.split("\n")
+
+max = {
+    "red": 12,
+    "green": 13,
+    "blue": 14,
+}
+
+game_regex = re.compile(r"Game (\d+): (.*)$")
+round_regex = re.compile(r"(\d+) (red|green|blue)")
+sum = 0
+for line in lines:
+    if not line:
+        continue
+    match = not_none(game_regex.match(line))
+    id = int(match.group(1))
+    text = match.group(2)
+    rounds = text.split(";")
+    is_possible = True
+    for round in rounds:
+        items = round.split(",")
+        for item in items:
+            match = not_none(round_regex.match(item.strip()))
+            if max[match.group(2)] < int(match.group(1)):
+                is_possible = False
+    if is_possible:
+        sum += id
+
+
+print(sum)
---
+++ aoc_2023/day_2/part_2.py
@@ -0,0 +1,34 @@
+from utils.file import read_file
+import re
+from functools import reduce
+
+from utils.types import not_none
+
+contents = read_file(__file__, "input")
+lines = contents.split("\n")
+
+game_regex = re.compile(r"Game (\d+): (.*)$")
+round_regex = re.compile(r"(\d+) (red|green|blue)")
+sum = 0
+for line in lines:
+    if not line:
+        continue
+    match = not_none(game_regex.match(line))
+    id = int(match.group(1))
+    text = match.group(2)
+    rounds = text.split(";")
+    mins = {
+        "red": 0,
+        "green": 0,
+        "blue": 0,
+    }
+    for round in rounds:
+        items = round.split(",")
+        for item in items:
+            match = not_none(round_regex.match(item.strip()))
+            mins[match.group(2)] = max(mins[match.group(2)], int(match.group(1)))
+    power = reduce(lambda a, b: a * b, mins.values())
+    sum += power
+
+
+print(sum)
