12f9dde9e33ea059c1b621230c2b3e97e279a49b
Ben Lu <ayroblu@gmail.com>
2023-12-11 11:10:11 +0000

    use zip

--- aoc_2023/day_11/part_2.py
+++ aoc_2023/day_11/part_2.py
@@ -9,7 +9,7 @@ expand_c: list[int] = [];
 for r, line in enumerate(lines):
     if all([l == "." for l in line]):
         expand_r.append(r)
-for c, char in enumerate(lines[0]):
+for c, char in enumerate(zip(*lines)):
     if all([l[c] == "." for l in lines if l[c]]):
         expand_c.append(c)
 
@@ -22,10 +22,9 @@ for r, line in enumerate(lines):
 expand = 1000000 - 1
 sum = 0
 for i, galaxy in enumerate(galaxies):
-    slice = galaxies[(i + 1):]
-    for j, g2 in enumerate(slice):
-        expand_rs = [r for r in expand_r if r < max(galaxy[0], g2[0]) and r > min(galaxy[0], g2[0])]
-        expand_cs = [c for c in expand_c if c < max(galaxy[1], g2[1]) and c > min(galaxy[1], g2[1])]
+    for g2 in galaxies[(i + 1):]:
+        expand_rs = [r for r in expand_r if min(galaxy[0], g2[0]) < r < max(galaxy[0], g2[0])]
+        expand_cs = [c for c in expand_c if min(galaxy[1], g2[1]) < c < max(galaxy[1], g2[1])]
         sum += abs(galaxy[0] - g2[0]) + abs(galaxy[1] - g2[1]) + len(expand_rs) * expand + len(expand_cs) * expand
 
 print(sum)
