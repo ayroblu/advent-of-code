ae54143eff8e7c188ab7e1bd66a70e86afbb00a8
Ben Lu <ayroblu@gmail.com>
2023-12-12 23:12:56 +0000

    refactor

--- aoc_2023/day_12/hot_springs.py
+++ aoc_2023/day_12/hot_springs.py
@@ -1,7 +1,7 @@
 import re
+
 from utils.file import read_file
 from utils.types import not_none
-
 
 contents = read_file(__file__, "input")
 lines = contents.split("\n")
@@ -14,21 +14,21 @@ for line in lines:
     meta = [int(i) for i in match.group(2).split(",")]
     possible_spring = [spring]
     for i, char in enumerate(spring):
-        if char != '?':
+        if char != "?":
             continue
         new_possible_spring: list[str] = []
         for spr in possible_spring:
             for s in [".", "#"]:
-                new_possible_spring.append(spr[:i] + s + spr[i + 1:])
+                new_possible_spring.append(spr[:i] + s + spr[i + 1 :])
         possible_spring = new_possible_spring
     running_sum = 0
     for s in possible_spring:
         count = 0
         agg: list[int] = []
         for char in s:
-            if char == '#':
+            if char == "#":
                 count += 1
-            elif char == '.' and count > 0:
+            elif char == "." and count > 0:
                 agg.append(count)
                 count = 0
         if count > 0:
