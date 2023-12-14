5ed9acce4eca55ba7159c108021269b6eeba6c96
Ben Lu <ayroblu@gmail.com>
2023-12-14 11:39:52 +0000

    clean up

--- aoc_2023/day_14/part_2.py
+++ aoc_2023/day_14/part_2.py
@@ -15,14 +15,9 @@ contents = read_input(__file__)
 stacks = list(map(list, contents.splitlines()))
 
 
-prev_layout = contents
-
-
 def cycle(stacks: list[list[str]]) -> list[list[str]]:
-    for c in range(4):
+    for _ in range(4):
         stacks = list(map(list, zip(*stacks)))
-        if c == 2 or c == 3:
-            stacks = [list(reversed(a)) for a in stacks]
         for line in stacks:
             for i, char in enumerate(line):
                 if char == "O":
@@ -33,32 +28,27 @@ def cycle(stacks: list[list[str]]) -> list[list[str]]:
                     if idx != i:
                         line[idx] = "O"
                         line[i] = "."
-        if c == 2 or c == 3:
-            stacks = [list(reversed(a)) for a in stacks]
+        stacks = [list(reversed(a)) for a in stacks]
     return stacks
 
 
-pattern: list[str] = []
-old_stacks: list[list[list[str]]] = []
-offset = 0
+seen: list[list[list[str]]] = []
 for i in range(1_000_000_000):
     stacks = cycle(stacks)
     layout = "\n".join(map(lambda x: "".join(x), stacks))
     # print(layout)
     # print("")
-    if layout in pattern:
-        index = pattern.index(layout)
-        diff = len(pattern) - index
+    if stacks in seen:
+        index = seen.index(stacks)
+        diff = len(seen) - index
         offset = (1_000_000_000 - index) % diff
-        stacks = old_stacks[index + offset - 1]
+        stacks = seen[index + offset - 1]
         break
-    pattern.append(layout)
-    old_stacks.append(stacks)
+    seen.append(stacks)
 
-# imagine 12 and 17 are matches, 1_000_000_000 would be (1e9 + 12) % diff
+# imagine 12 and 17 are matches, 1_000_000_000 would be (1e9 - 12) % (17-12)
 # 12 + 5*x > 1e9
-# 1e9 - 12 / 5
-# diff * x + offset = 1e9
+# (1e9 - 12) / 5
 
 sum = 0
 for i, line in enumerate(stacks):
