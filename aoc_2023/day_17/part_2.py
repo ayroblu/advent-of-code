eb3d0a334d377870029180fa99ec1d9d7667f720
Ben Lu <ayroblu@gmail.com>
2023-12-17 15:12:41 +0000

    day 17

---
+++ aoc_2023/day_17/part_2.py
@@ -0,0 +1,83 @@
+from bisect import insort
+
+from utils.file import read_input
+
+contents = read_input(__file__)
+# contents = """2413432311323
+# 3215453535623
+# 3255245654254
+# 3446585845452
+# 4546657867536
+# 1438598798454
+# 4457876987766
+# 3637877979653
+# 4654967986887
+# 4564679986453
+# 1224686865563
+# 2546548887735
+# 4322674655533"""
+
+grid = contents.splitlines()
+
+type CoordWithSum = tuple[
+    tuple[int, int], tuple[int, int], int, int, list[tuple[int, int]]
+]
+seen: dict[tuple[int, int], list[tuple[int, tuple[tuple[int, int], int]]]] = {}
+
+nodes: list[CoordWithSum] = [((0, 0), (0, 1), 0, 0, []), ((0, 0), (1, 0), 0, 0, [])]
+while len(nodes):
+    (r, c), (rd, cd), cost, same_dir, history = nodes.pop()
+
+    for nrd, ncd in [(1, 0), (-1, 0), (0, -1), (0, 1)]:
+        if rd == nrd and cd == ncd and same_dir == 10:
+            continue
+        if rd == -nrd and cd == -ncd:
+            continue
+        nr = r + nrd
+        nc = c + ncd
+        if rd != nrd or cd != ncd:
+            # if turning, need to check 4 ahead
+            nr = r + nrd * 4
+            nc = c + ncd * 4
+        if nr >= len(grid) or nr < 0 or nc >= len(grid[0]) or nc < 0:
+            continue
+        new_cost = cost + int(grid[nr][nc])
+        if rd != nrd or cd != ncd:
+            # also adjust the cost
+            new_cost = cost
+            for i in range(4):
+                new_cost += int(grid[r + nrd * (i + 1)][c + ncd * (i + 1)])
+        dir = 4
+        if rd == nrd and cd == ncd:
+            dir = same_dir + 1
+        done = False
+        if (nr, nc) in seen:
+            for seen_cost, (seen_dir, num_dir) in seen[(nr, nc)]:
+                if seen_cost <= new_cost and (nrd, ncd) == seen_dir and num_dir <= dir:
+                    done = True
+                    break
+            else:
+                seen[(nr, nc)].append((new_cost, ((nrd, ncd), dir)))
+        else:
+            seen[(nr, nc)] = [(new_cost, ((nrd, ncd), dir))]
+        if done:
+            continue
+        if nr == len(grid) - 1 and nc == len(grid[0]) - 1:
+            # new_grid = [[c for c in line] for line in contents.splitlines()]
+            # for r, c in history:
+            #     new_grid[r][c] = "x"
+            # result = "\n".join(["".join(line) for line in new_grid])
+
+            # print(cost)
+            # print(result)
+            break
+        insort(
+            nodes,
+            ((nr, nc), (nrd, ncd), new_cost, dir, history + [(nr, nc)]),
+            key=(lambda v: -v[2]),
+        )
+    else:
+        continue
+    break
+
+print("cost", seen[(len(grid) - 1, len(grid[0]) - 1)][0][0])
