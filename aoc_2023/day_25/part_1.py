613a8411c80e79486d1113a36098c20b3cbd017b
Ben Lu <ayroblu@gmail.com>
2023-12-25 22:28:06 +0000

    simplify

--- aoc_2023/day_25/part_1.py
+++ aoc_2023/day_25/part_1.py
@@ -31,35 +31,28 @@ start_node = next(iter(graph))
 
 def cut(start_node: str):
     seen: set[str] = {start_node}
-    stack: list[str] = []
     possible: dict[str, int] = defaultdict(lambda: 0, {k: 1 for k in graph[start_node]})
+    min_cut = None
     while len(seen) < len(graph):
+        total = sum(possible.values())
+        if min_cut == None:
+            min_cut = total
+        min_cut = min(total, min_cut)
+        if total == 3:
+            print(len(seen), len(seen) * (len(graph) - len(seen)))
+            return
+
         next_node, _ = max([(k, v) for k, v in possible.items()], key=lambda x: x[1])
+
         seen.add(next_node)
-        stack.append(next_node)
         possible.pop(next_node)
         for p in graph[next_node]:
             if p in seen:
                 continue
             possible[p] += 1
 
-    other: set[str] = set()
-    while len(stack):
-        next_node = stack.pop()
-        seen.remove(next_node)
-        other.add(next_node)
-        count = 0
-        for node in other:
-            for con in graph[node]:
-                if con in seen:
-                    count += 1
-        if count == 3:
-            print(len(other), len(seen), len(other) * len(seen))
-            return
-
 
 # Add all nodes in a stack, pick most "connected" each time
-# pop back through the stack till num edges = 3
 
 
 cut(start_node)
