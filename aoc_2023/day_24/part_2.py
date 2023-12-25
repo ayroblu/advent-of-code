0d31e0e7c2bc489c17de12969ce648e52240a63f
Ben Lu <ayroblu@gmail.com>
2023-12-25 10:35:54 +0000

    sympy implementation

--- aoc_2023/day_24/part_2.py
+++ aoc_2023/day_24/part_2.py
@@ -1,7 +1,5 @@
-from typing import Any
-
 import numpy as np
-from numpy.typing import NDArray
+import sympy
 from utils.file import read_input
 
 contents = read_input(__file__)
@@ -31,20 +29,42 @@ for line in contents.splitlines():
 # - p[i] x vr + p[i2] x vr + v[i] x pr - v[i2] x pr == p[i2] x v[i2] - p[i] x v[i]
 
 
-A = np.zeros((6, 6))
-p0, v0 = pos_vecs[0]
-p1, v1 = pos_vecs[1]
-p2, v2 = pos_vecs[2]
-A[:3, :3] = np.cross(np.identity(3), v0) - np.cross(np.identity(3), v1)
-A[:3, 3:] = -np.cross(np.identity(3), p0) + np.cross(np.identity(3), p1)
-A[3:, :3] = np.cross(np.identity(3), v0) - np.cross(np.identity(3), v2)
-A[3:, 3:] = -np.cross(np.identity(3), p0) + np.cross(np.identity(3), p2)
+def linarg_solve():
+    A = np.zeros((6, 6))
+    p0, v0 = pos_vecs[0]
+    p1, v1 = pos_vecs[1]
+    p2, v2 = pos_vecs[2]
+    A[:3, :3] = np.cross(np.identity(3), v0) - np.cross(np.identity(3), v1)
+    A[:3, 3:] = -np.cross(np.identity(3), p0) + np.cross(np.identity(3), p1)
+    A[3:, :3] = np.cross(np.identity(3), v0) - np.cross(np.identity(3), v2)
+    A[3:, 3:] = -np.cross(np.identity(3), p0) + np.cross(np.identity(3), p2)
 
-b = np.zeros(6)
-b[:3] = np.cross(np.array(p1), np.array(v1)) - np.cross(np.array(p0), np.array(v0))
-b[3:] = np.cross(np.array(p2), np.array(v2)) - np.cross(np.array(p0), np.array(v0))
-result = np.linalg.solve(A, b)
+    b = np.zeros(6)
+    b[:3] = np.cross(np.array(p1), np.array(v1)) - np.cross(np.array(p0), np.array(v0))
+    b[3:] = np.cross(np.array(p2), np.array(v2)) - np.cross(np.array(p0), np.array(v0))
+    result = np.linalg.solve(A, b)
 
-print(result)
-# print(A)
-print(round(np.sum(result[:3])))
+    # print(A)
+    print(result)
+    print(round(np.sum(result[:3])))
+
+
+linarg_solve()
+
+
+def symbolic_solve():
+    # No types for sympy
+    xr, yr, zr, vxr, vyr, vzr = sympy.symbols("xr, yr, zr, vxr, vyr, vzr")
+
+    equations = []
+    for (px, py, pz), (vx, vy, vz) in pos_vecs:
+        equations.append((xr - px) * (vy - vyr) - (yr - py) * (vx - vxr))
+        equations.append((yr - py) * (vz - vzr) - (zr - pz) * (vy - vyr))
+
+    result = sympy.solve(equations)[0]
+    answer = sum(result[v] for v in [xr, yr, zr])
+    print(result)
+    print(answer)
+
+
+symbolic_solve()
