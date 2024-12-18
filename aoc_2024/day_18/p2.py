67dd1c0a1b2caf1155fe1f0999b7c9230c665684
Ben Lu <ayroblu@gmail.com>
2024-12-18 21:34:17 +0000

    day 18

---
+++ aoc_2024/day_18/p2.py
@@ -0,0 +1,54 @@
+from bisect import bisect_right
+
+from utils.file import read_input
+
+contents = read_input(__file__).strip()
+lines = contents.splitlines()
+
+maxn = 70
+type Point = tuple[int, int]
+
+
+def run(dropped: int) -> int:
+    affected = lines[: dropped + 1]
+
+    corrupted = set[Point]()
+    for line in affected:
+        r, c = map(int, line.split(","))
+        corrupted.add((r, c))
+
+    seen = set[Point]()
+    poss = [(0, 0)]
+    end = (maxn, maxn)
+    dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+    dist = 0
+    while len(poss) > 0:
+        dist += 1
+        next_poss: list[Point] = []
+        for r, c in poss:
+            for dr, dc in dirs:
+                r1, c1 = r + dr, c + dc
+                if (r1, c1) == end:
+                    return 0
+
+                if not (0 <= r1 <= maxn):
+                    continue
+                if not (0 <= c1 <= maxn):
+                    continue
+                if (r1, c1) in corrupted:
+                    continue
+
+                if (r1, c1) in seen:
+                    continue
+                seen.add((r1, c1))
+
+                next_poss.append((r1, c1))
+        poss = next_poss
+    return 1
+
+
+idx = bisect_right(range(len(lines)), 0, key=run)
+print(lines[idx])
+print(idx)
+assert run(idx - 1) == 0
+assert run(idx) == 1
